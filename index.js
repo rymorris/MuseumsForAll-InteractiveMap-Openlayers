a94d13e793855fe805e960481e2c3483133dc375
rymorris <rymorris1@gmail.com>
2025-03-25 11:54:24 -0700

    UI fixes

--- index.js
+++ index.js
@@ -228,6 +228,13 @@ document.addEventListener('DOMContentLoaded', function () {
                           cityInfo.textContent = `${city.city}, ${city.state} `;
                           cityInfo.style.cursor = 'pointer';
                           cityInfo.addEventListener('click', () => {
+                            // Set the input value to the selected city
+                            cityInput.value = `${city.city}, ${city.state}`;
+
+                            // Hide the results after selection
+                            resultsDiv.innerHTML = '';
+
+                            // Center the map on the selected city
                             if (typeof city.longitude === 'number' && typeof city.latitude === 'number') {
                               map.getView().animate({
                                   center: [city.longitude, city.latitude],
